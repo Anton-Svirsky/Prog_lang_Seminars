3353de49e2918c33cb2df6b7cb7dfaab2f44bf3a
Anton <anton_svirsky@mail.ru>
2022-06-23 23:51:43 +0300

    add solved Seminar1_Task3

---
+++ Seminar1_Task3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 2 == 0)
+    Console.WriteLine(number + " - > Yes ");
+else
+    Console.WriteLine(number + " - > No");
