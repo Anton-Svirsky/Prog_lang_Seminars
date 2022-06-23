9ec9ebae347b21a034c4591b3d25627ca787268e
Anton <anton_svirsky@mail.ru>
2022-06-23 23:19:53 +0300

    Seminar1_Task1 is solved

--- Seminar1_Task1/Program.cs
+++ Seminar1_Task1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+    Console.WriteLine("number1 = " + number1 + ", number2 = " + number2 + " - > max = " + number1);
+    else if(number1 == number2)
+        Console.WriteLine("number1 = " + number1 + ", number2 = " + number2 + " - > there is no max");
+else
+    Console.WriteLine("number1 = " + number1 + ", number2 = " + number2 + " - > max = " + number2);
