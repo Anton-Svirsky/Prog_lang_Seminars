c11e7df5df3787cd26b98fac792e7bf7b3962e34
Anton <anton_svirsky@mail.ru>
2022-06-23 23:38:44 +0300

    add solved Seminar1_Task2

---
+++ Seminar1_Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1; 
+if(number1>max) max = number1;
+if(number2>max) max = number2;
+if(number3>max) max = number3;
+Console.WriteLine(number1 + " " + number2 + " " + number3 + " - > " + max);
