fc371a0572216689f49419bd4eab594f23decdc8
Anton <anton_svirsky@mail.ru>
2022-06-24 08:02:23 +0300

    add Seminnar1Task4

---
+++ Seminnar1Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int oneline = 0;
+for (int count = 1; count < number; count++)
+{
+    if(count % 2 == 0)
+    {
+        oneline = count;
+    }
+}
+ Console.WriteLine(number + " - > " + oneline);
