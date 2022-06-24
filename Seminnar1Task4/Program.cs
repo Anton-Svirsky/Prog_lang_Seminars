8eff0f9dbe5122f050c02c5fe8089d5c6a5dff75
Anton <anton_svirsky@mail.ru>
2022-06-24 11:32:50 +0300

    Seminnar1Task4 solution added

--- Seminnar1Task4/Program.cs
+++ Seminnar1Task4/Program.cs
@@ -1,11 +1,9 @@
 ﻿Console.Write("Input number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int oneline = 0;
-for (int count = 1; count < number; count++)
+string even_numbers = " - > ";
+for (int count = 1; count <= number; count++)
 {
-    if(count % 2 == 0)
-    {
-        oneline = count;
-    }
+     if (count % 2 == 0)
+    even_numbers = even_numbers + count + ", ";
 }
- Console.WriteLine(number + " - > " + oneline);
+ Console.WriteLine(number + even_numbers);
