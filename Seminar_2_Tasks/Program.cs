c2c6fe9304fedd6a7a2cab923b0483927e23152d
Anton <anton_svirsky@mail.ru>
2022-07-02 18:47:48 +0300

    add Seminar_2_Tasks

---
+++ Seminar_2_Tasks/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Console.WriteLine("Enter three digits number: ");
+// int Number = Convert.ToInt32(Console.ReadLine());
+// string stringNumber = Convert.ToString(Number);
+// Console.WriteLine(Number + " -> "+stringNumber[1]);
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Console.WriteLine("Enter a number:");
+// int Number = Convert.ToInt32(Console.ReadLine());
+// string number = Convert.ToString(Number);
+// if (number.Length > 2)
+//     Console.WriteLine(Number + " -> " + number[2]);
+// else
+//     Console.WriteLine("There is no third number");
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Enter a number, which points a day of the week:");
+int DayOfTheWeek = Convert.ToInt32(Console.ReadLine());
+void DayCheck (int DayOfTheWeek) {
+    if (DayOfTheWeek == 6 || DayOfTheWeek == 7)
+        Console.WriteLine(DayOfTheWeek + " ->" + " Yeap");
+    else if (DayOfTheWeek < 1 || DayOfTheWeek > 7)
+        Console.WriteLine("This is not even a day");
+    else Console.WriteLine(DayOfTheWeek + " ->" + " Nope");
+}
+DayCheck(DayOfTheWeek);
--- Seminnar1Task4/Program.cs
+++ Seminnar1Task4/Program.cs
@@ -2,12 +2,9 @@
 int number = Convert.ToInt32(Console.ReadLine());
 string even_numbers = " - > ";
 for (int count = 1; count <= number; count++)
-{
-    if (count = %2)
+    if (count == count % 2)
     {
-        count = count + count;
-        even_numbers = even_numbers + count + ", ";
+    count = count + count;
+    even_numbers = even_numbers + count + ", ";
     }
-
-}
 Console.WriteLine(number + even_numbers);
