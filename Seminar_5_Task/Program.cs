6f7367d11c7554cfc6a51332f809c023650adf75
Anton <anton_svirsky@mail.ru>
2022-07-13 21:34:13 +0300

    add Seminar_5_Task

---
+++ Seminar_5_Task/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+// int[] CreateRandomArray(int N, int start, int end)
+// {
+//     int[] RandomArray = new int[N];
+//     for (int i = 0; i < N; i++)
+//     {
+//         RandomArray[i] = new Random().Next(start, end + 1);
+//     }
+//     return RandomArray;
+// }
+
+// void ShowArray(int[] array)
+// {
+//     Console.Write("[ ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.Write("]");
+// }
+// int findQuontityOfPositive(int[] array)
+// {
+//     int amount = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] % 2 == 0)
+//         {
+//             amount++;
+//         }
+//     }
+//     return amount;
+// }
+// int[] array = CreateRandomArray(10, 100, 998);
+// int quontity = findQuontityOfPositive(array);
+// ShowArray(array);
+// Console.Write($" - > {quontity}");
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
