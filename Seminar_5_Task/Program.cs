7ef3eb92d9db6aa188ccaa14e6a4492976188de8
Anton <anton_svirsky@mail.ru>
2022-07-18 08:03:08 +0300

    add Seminar_6_Tasks

--- Seminar_5_Task/Program.cs
+++ Seminar_5_Task/Program.cs
@@ -118,3 +118,60 @@
 // int difference = findDifference(array);
 // ShowArray(array);
 // Console.Write($" - > {difference}");
+
+
+Console.Clear();
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int row = 0; row < inArray.GetLength(0); row++)
+    {
+        for (int column = 0; column < inArray.GetLength(1); column++)
+        {
+            Console.Write($"\t {inArray[row, column]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void changedArray(double[,] inArray)
+{
+
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                inArray[i, j] = Math.Pow(inArray[i, j], 2);
+            }
+            Console.Write($"\t {inArray[i, j]}");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+changedArray(array);
---
+++ Seminar_6_Tasks/Program.cs
@@ -0,0 +1,72 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// Console.Write($"Enter amount of numbers: ");
+// int Amount = Convert.ToInt32(Console.ReadLine());
+// int[] arrayOfNumbers = new int[Amount];
+
+// void InputNumbers(int Amount)
+// {
+//     for (int i = 0; i < Amount; i++)
+//     {
+//         Console.Write($"Enter {i + 1} number: ");
+//         arrayOfNumbers[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+// }
+// void ShowArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " , ");
+//     }
+
+// }
+// int Compare(int[] arrayOfNumbers)
+// {
+//     int count = 0;
+//     for (int i = 0; i < arrayOfNumbers.Length; i++)
+//     {
+//         if (arrayOfNumbers[i] > 0) count += 1;
+//     }
+//     return count;
+// }
+
+// InputNumbers(Amount);
+// ShowArray(arrayOfNumbers);
+// Console.Write($" -> {Compare(arrayOfNumbers)}");
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+// void FindingInterception()
+// {
+//     Console.WriteLine("Enter k1 in y = k1 * x + b1:");
+//     double k1 = Convert.ToDouble(Console.ReadLine());
+//     Console.WriteLine("Enter b1 in y = k1 * x + b1:");
+//     double b1 = Convert.ToDouble(Console.ReadLine());
+//     Console.WriteLine("Enter k2 in y = k2 * x + b2:");
+//     double k2 = Convert.ToDouble(Console.ReadLine());
+//     Console.WriteLine("Enter b2 in y = k2 * x + b2:");
+//     double b2 = Convert.ToDouble(Console.ReadLine());
+
+//     if (k1 == k2)
+//     {
+//         if (b1 == b2)
+//         {
+//             Console.WriteLine("there are infinitely many points of intersection - the lines coincide");
+//         }
+//         else
+//         {
+//             Console.WriteLine("there are no points of intersection - the lines are parallel");
+//         }
+//     }
+//     else
+//     {
+//         Console.Write($"b1={b1}, k1={k1}, b2={b2}, k2={k2}");
+//         double x = (b1 - b2) / (k2 - k1);
+//         double y = k1 * x + b1;
+//         Console.Write($" - > {x}; {y}");
+//     }
+// }
+// FindingInterception();
