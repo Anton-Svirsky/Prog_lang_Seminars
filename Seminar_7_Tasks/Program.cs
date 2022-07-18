4c4a3fc39d4ca4e2dc539194d11355ec018b5a66
Anton <anton_svirsky@mail.ru>
2022-07-18 17:35:51 +0300

    add Seminar_7_Tasks

---
+++ Seminar_7_Tasks/Program.cs
@@ -0,0 +1,83 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// double[,] GetArray(int m, int n)
+// {
+//     double[,] result = new double[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().NextDouble() * 100 - 50;
+//             result[i, j] = Math.Round(result[i, j], 2);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(double[,] inArray)
+// {
+//     for (int row = 0; row < inArray.GetLength(0); row++)
+//     {
+//         for (int column = 0; column < inArray.GetLength(1); column++)
+//         {
+//             Console.Write($"{inArray[row, column]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// Console.Write("Enter number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Enter number of columns: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// double[,] array = GetArray(rows, columns);
+// PrintArray(array);
+
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int row = 0; row < inArray.GetLength(0); row++)
+    {
+        for (int column = 0; column < inArray.GetLength(1); column++)
+        {
+            Console.Write($"\t {inArray[row, column]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+double [,] MyArray = GetArray(4,4,-10,10);
+PrintArray(MyArray);
+Console.Write("Enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
