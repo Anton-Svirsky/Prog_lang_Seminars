ca2c2e21eab8e28e21a6724662ff9366124ca208
Anton <anton_svirsky@mail.ru>
2022-07-25 19:06:53 +0300

    add Seminar_8 & Seminar2

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,18 +1,18 @@
-﻿void randomNumber(int min, int max){
-   int number = new Random().Next(min, max+1);
-   Console.Write(number + " -> ");
-    if (number / 10 > number % 10){
-       Console.WriteLine(number / 10);
-    }
-    else{
-       Console.WriteLine(number % 10);
-    }
-}
+﻿// void randomNumber(int min, int max){
+//    int number = new Random().Next(min, max+1);
+//    Console.Write(number + " -> ");
+//     if (number / 10 > number % 10){
+//        Console.WriteLine(number / 10);
+//     }
+//     else{
+//        Console.WriteLine(number % 10);
+//     }
+// }
 
-Console.WriteLine("Введите минимальное число диапазона: ");
-int start = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите максимальное число диапазона: ");
-int end = Convert.ToInt32(Console.ReadLine());
-randomNumber(start, end);
+// Console.WriteLine("Введите минимальное число диапазона: ");
+// int start = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимальное число диапазона: ");
+// int end = Convert.ToInt32(Console.ReadLine());
+// randomNumber(start, end);
 
 
